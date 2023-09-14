3f94007e354ae64090c627e50668371f4b0f87a6
Edilson Moizinho <edilsondiasalves@outlook.com>
2023-09-14 00:58:54 -0300

    Polishing the UI

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -13,17 +13,18 @@ import com.example.affirmations.model.Affirmation
 /**
  * Adapter for the [RecyclerView] in [MainActivity]. Displays [Affirmation] data object.
  */
-class ItemAdapter(private val context: Context,
-                  private val dataset : List<Affirmation>
-    ): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>()  {
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just an Affirmation object.
-    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
-            val textView: TextView = view.findViewById(R.id.item_title)
-            val imageView: ImageView = view.findViewById(R.id.item_image)
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     /**
